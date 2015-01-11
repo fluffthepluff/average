d6a1bf352489ac63fcde2c1d1fbac5d17305afc4
fluffthepluff <wilf361@outlook.com>
2015-01-11 21:00:33 +0000

    Ask for the numbers and calc how many there are
    Use (read-line) to get numbers from the user, and convert to a list
    using (string-split).
    
    Calculate the length of the list.

--- averages.rkt
+++ averages.rkt
@@ -2,3 +2,6 @@
 #|
 Average: Calculates the mean of a list of values
 |#
+
+
+(define (numbers) (displayln "Please enter a list of numbers that you want to find the average of seperated by spaces:") (length (string-split (read-line))))
